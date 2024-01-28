db937f16151bd317f467c6bc24440cdbcadf38d2
ruhaimmohammed <ruhaimmohammedk@gmail.com>
2024-01-29 01:33:43 +0530

    linear search

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import isPrime from './isPrime.js';
 import isPowerOfTwo, { isPowerOfTwoBitWise } from './isPowerOfTwo.js';
 import recursiveFibanocci from './recursiveFibanocci.js';
 import recursiveFactorial from './recursiveFactorial.js';
+import linearSearch from './linearSearch.js';
 
 // fibanocci(7);
 // factorial(5);
@@ -12,3 +13,5 @@ import recursiveFactorial from './recursiveFactorial.js';
 // console.log(isPowerOfTwoBitWise(8));
 // console.log(recursiveFibanocci(50));
 // console.log(recursiveFactorial(5)); // 120 
+console.log(linearSearch([0,2,4,5,3],5)); // 3
+console.log(linearSearch([0,2,4,5,3],50)); // -1
---
+++ linearSearch.js
@@ -0,0 +1,12 @@
+const linearSearch = (arr, target) => {
+    for (let i = 0; i <= arr.length; i++) {
+        if(arr[i] === target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+export default linearSearch;
+
+// Big O = O(n)
