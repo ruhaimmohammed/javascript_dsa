e1ca09e605677b1d032a40fb2c000d3ebfd7b9db
ruhaimmohammed <ruhaimmohammedk@gmail.com>
2024-01-31 01:41:25 +0530

    recursive binary search

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import recursiveFibanocci from './recursiveFibanocci.js';
 import recursiveFactorial from './recursiveFactorial.js';
 import linearSearch from './linearSearch.js';
 import binarySearch from './binarySearch.js';
+import recursiveBinarySearch from './recursiveBinarySearch.js';
 
 // fibanocci(7);
 // factorial(5);
@@ -16,5 +17,7 @@ import binarySearch from './binarySearch.js';
 // console.log(recursiveFactorial(5)); // 120 
 // console.log(linearSearch([0,2,4,5,3],5)); // 3
 // console.log(linearSearch([0,2,4,5,3],50)); // -1
-console.log(binarySearch([-5, 2, 4, 6, 10], -5)); //0
-console.log(binarySearch([-5, 2, 4, 6, 10], 10)); //4
+// console.log(binarySearch([-5, 2, 4, 6, 10], -5)); //0
+// console.log(binarySearch([-5, 2, 4, 6, 10], 10)); //4
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], -5)); //0
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10)); //4
---
+++ recursiveBinarySearch.js
@@ -0,0 +1,22 @@
+const recursiveBinarySearch = (arr, target) => {
+    return search(arr, target, 0, arr.length - 1);
+}
+
+const search = (arr, target, leftIndex, rightIndex) => {
+    if (leftIndex > rightIndex) {
+        return -1;
+    }
+
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) {
+        return middleIndex;
+    }
+
+    if (target < arr[middleIndex]) {
+        return search(arr, target, leftIndex, middleIndex - 1);
+    } else {
+        return search(arr, target, middleIndex + 1, rightIndex);
+    }
+}
+
+export default recursiveBinarySearch;
